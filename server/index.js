6800e41bc7cc905f68cbd6ee1f2c93bd37f44be7
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-21 22:44:21 +0800

    Connected dalle route to front end

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv'; //for managing secrets
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
-dotenv.config();
+dotenv.config(); dotenv.config(); //pools variable from dotenv files
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+// api endpoints
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
   res.status(200).json({
-    message: 'Hello from DALL.E!',
+    message: 'Hello from DALL!',
   });
 });
 
